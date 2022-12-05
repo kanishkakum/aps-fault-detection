6a73d617c6e333ed3e4cf60d6c1c5b576c7437d1
kanishkakum <kanishkakumar408@gmail.com>
2022-12-06 00:02:19 +0530

    created artifact dir and data ingestion directory

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -1,8 +1,25 @@
-class DataIngestionConfig:...
+import os
+
+class TrainingPipelineConfig:
+    def __init__(self):
+        self.artifact_dir=os.path.join(os.getcwd(),"artifact",f"{datetime.now().strftime('%m%d%Y__%H%M%S')}")
+
+
+class DataIngestionConfig:
+
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.database_name="aps"
+        self.collection_name = "sensor"
+        self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir, "data_ingestion")
+
+
+
+
+
 class DataValidationConfig:...
 class DataTransformationConfig:...
 class ModelTrainerConfig:...
 class ModelEvaluationConfig:...
 class ModelPusherConfig:...
 
-class TrainingPipelineConfig:...
+
