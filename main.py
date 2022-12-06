e48e8831c2159cc2b2fa49ee12dbc424899c15cb
kanishkakum <kanishkakumar408@gmail.com>
2022-12-07 01:04:44 +0530

    data ingestion directory for train, test csv

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from sensor.logger import logging
 from sensor.exception import SensorException
 from sensor.utils import get_collection_as_dataframe
+from sensor.entity import config_entity
 import sys,os
 
 if __name__=="__main__":      
      try:
-          get_collection_as_dataframe(database_name="aps",database_collection="sensor")
+          training_pipeline_config= config_entity.TrainingPipelineConfig()
+          data_ingestion_config= config_entity.DataIngestionConfig(training_pipeline_config)
+          print(data_ingestion_config.to_dict())
      except Exception as e:
           print(e)           
