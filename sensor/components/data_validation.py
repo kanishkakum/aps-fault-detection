40cf608ad6c3e7042250551cb892a2239c67026e
kanishkakum <kanishkakumar408@gmail.com>
2022-12-15 02:33:18 +0530

    drop missing values column with threshold of missing value

--- sensor/components/data_validation.py
+++ sensor/components/data_validation.py
@@ -3,6 +3,7 @@ from sensor.exception import SensorException
 from sensor.logger import logger
 import os,sys
 from scipy.stats import ks2_samp
+import pandas as pd
 
 class DataValidation:
 
@@ -12,7 +13,35 @@ class DataValidation:
             self.data_validation_config=data_validation_config
 
         except:
-            raise SensorException(e, sys)    
+            raise SensorException(e, sys)  
+
+
+    def is_existing_columns_exist(self,)->bool:
+        pass
+
+    def drop_missing_values_column(self,df:pd.DataFrame,threshold:float=0.3)->Option[pd.DataFrame]:
+        """ 
+        This function basically removes those columns which have more than 30% missing values
+        """
+        try:
+            null_report = df.isna().sum()/df.shape[0]
+            #selecting column name which contain null
+            drop_column_names = null_report[null_report>threshold].index
+            df.drop(list(drop_column_names),axis=1,inplace=True)
+
+            #return None if no column left
+            if len(df.column)==0:
+                return None
+            return df
+                
+
+        except:
+            raise SensorException(e, sys)
+
+
+
+
+
 
 
     def initiate_data_validation(self)->artifact_entity.DataValidationArtifact:
