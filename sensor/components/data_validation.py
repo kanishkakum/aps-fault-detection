b836d5f7a6f71db2fc7d2970e9db7738e8d587c6
kanishkakum <kanishkakumar408@gmail.com>
2022-12-16 00:42:24 +0530

    checking required column exist or not

--- sensor/components/data_validation.py
+++ sensor/components/data_validation.py
@@ -11,22 +11,22 @@ class DataValidation:
         try:
             logging.info(f"{'>>'*20} Data Validation {'<<'*20}")
             self.data_validation_config=data_validation_config
+            self.validation_error=dict()
 
         except:
             raise SensorException(e, sys)  
 
 
-    def is_existing_columns_exist(self,)->bool:
-        pass
-
-    def drop_missing_values_column(self,df:pd.DataFrame,threshold:float=0.3)->Option[pd.DataFrame]:
+    def drop_missing_values_column(self,df:pd.DataFrame)->Option[pd.DataFrame]:
         """ 
         This function basically removes those columns which have more than 30% missing values
         """
         try:
+            threshold=self.data_validation_config.missing_threshold
             null_report = df.isna().sum()/df.shape[0]
             #selecting column name which contain null
             drop_column_names = null_report[null_report>threshold].index
+            self.validation_error["dropped columns"]=drop_column_names
             df.drop(list(drop_column_names),axis=1,inplace=True)
 
             #return None if no column left
@@ -39,10 +39,40 @@ class DataValidation:
             raise SensorException(e, sys)
 
 
+    def is_required_columns_exist(self,base_df:pd.DataFrame,current_df:pd.DataFrame)->bool:
+        try:
+            
+            base_columns=base_df.columns
+            current_columns=current_df.columns
+
+            missing_columns=[]
+            for base_column in base_columns:
+                if base_column not in current_columns:
+                    missing_columns.append(base_column)
+
+            if len(missing_columns)>0:
+                self.validation_error["Missing column"]=missing_columns
+                return False
+            return True     
+        except:
+            raise e        
 
 
+    
+    def data_drift(self,base_df:pd.DataFrame,current_df:pd.DataFrame):
+        try:
+            base_columns=base_df.columns
+            current_columns=current_df.columns
+            for base_column in base_columns:
+                base_data,current_data=base_df[base_column],current_df[base_column]
+                same_distribution=ks2_samp(base_data,current_data)
+                if same_distribution.pvalue>0.05:
+                    pass
+                    #same distribution
+                else:
+                    pass
+                    #other distribution    
 
 
 
     def initiate_data_validation(self)->artifact_entity.DataValidationArtifact:
-
