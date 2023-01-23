9a6d5c1147abce0723b002b05dacf1249c04a9fd
kanishkakum <kanishkakumar408@gmail.com>
2023-01-24 01:03:58 +0530

    model evaluation continue.....

--- sensor/components/model_evaluation.py
+++ sensor/components/model_evaluation.py
@@ -70,3 +70,22 @@ class ModelEvaluation:
             print(f"Prediction using previous model: {target_encoder.inverse_transform(y_pred[:5])}")
             previous_model_score = f1_score(y_true=y_true, y_pred=y_pred)
             logging.info(f"Accuracy using previous trained model: {previous_model_score}")
+
+            # accuracy using current trained model
+            input_feature_name = list(current_transformer.feature_names_in_)
+            input_arr =current_transformer.transform(test_df[input_feature_name])
+            y_pred = current_model.predict(input_arr)
+            y_true =current_target_encoder.transform(target_df)
+            print(f"Prediction using trained model: {current_target_encoder.inverse_transform(y_pred[:5])}")
+            current_model_score = f1_score(y_true=y_true, y_pred=y_pred)
+            logging.info(f"Accuracy using current trained model: {current_model_score}")
+            if current_model_score<=previous_model_score:
+                logging.info(f"Current trained model is not better than previous model")
+                raise Exception("Current trained model is not better than previous model")
+
+            model_eval_artifact = artifact_entity.ModelEvaluationArtifact(is_model_accepted=True,
+            improved_accuracy=current_model_score-previous_model_score)
+            logging.info(f"Model eval artifact: {model_eval_artifact}")
+            return model_eval_artifact
+        except Exception as e:
+            raise SensorException(e,sys)
